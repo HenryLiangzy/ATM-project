d42fa6ea62948417abcd6006c60089a4b3072cd5
Henry Liang <HenryLiangzy@gmail.com>
2016-12-05 14:47:59 +0800

    add more sub-function and add more detail to function_list function

--- ATM project/main.cpp
+++ ATM project/main.cpp
@@ -27,10 +27,78 @@ int login()
 	//login function will return the account ID back to the main function
 }
 
+void check(int id)
+{
+	//
+}
+
+void save(int id)
+{
+	//
+}
+
+
+void withdraw(int id)
+{
+
+}
+
+void transfer(int id)
+{
+	//
+}
+
+void quit()
+{
+	//quit function used to end the program
+	exit(0);
+}
+
+
+
+
 void function_list(int id)
 {
 	//here is for the function_list
 	//input the function number and jump to that funcion
+	int choose;
+	do
+	{
+		printf("\t\t|$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$|\n");
+		printf("\t\t|                                               |\n");
+		printf("\t\t|       Welcome to Use ATM System         	|\n");
+		printf("\t\t|                                               |\n");
+		printf("\t\t|    Please Select The Following Functions:     |\n");
+		printf("\t\t|                                               |\n");
+		printf("\t\t|   $$-Check	 -- Press 1			|\n");
+		printf("\t\t|   $$-Save      -- Press 2			|\n");
+		printf("\t\t|   $$-Withdraw	 -- Press 3			|\n");
+		printf("\t\t|   $$-Transfer	 -- Press 4			|\n");
+		printf("\t\t|   $$-Quit      -- Press 5			|\n");
+		printf("\t\t|                                               |\n");
+		printf("\t\t|$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$|\n");
+
+		printf("\n\n\n\n");
+
+		printf("Please Input Number:");
+		scanf("%d", &choose);
+
+		switch (choose)
+		{
+		case 1:check(id);
+			break;
+		case 2:save(id);
+			break;
+		case 3:withdraw(id);
+			break;
+		case 4:transfer(id);
+			break;
+		case 5:quit();
+			break;
+		default:printf("Please input the number 1~5\n");
+			break;
+		}
+	} while (1);
 }
 
 void main()
