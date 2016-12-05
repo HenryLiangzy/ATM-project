35290e290683cce9a47ff871763a3bb135b5ddd6
Henry Liang <HenryLiangzy@gmail.com>
2016-12-05 13:35:41 +0800

    create the ATM projrct

---
+++ ATM project/main.cpp
@@ -0,0 +1,28 @@
+/*
+C programming file for ATM project
+By Henry Liang    ID:1530003030
+Test on Visual Studio Community 2015, Windows 10 v1607
+Last modification: Dec 5, 2016
+*/
+
+#define _CRT_SECURE_NO_DEPRECATE
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct {
+	int id;
+	char name[16];
+	char password[8];
+	float balance;
+	char level;
+}bankCount;
+//定义结构体bankCount
+
+
+void main()
+{
+
+}
