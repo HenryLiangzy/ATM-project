55d783af7e6572515e071843496b8e729e0cb180
Henry Liang <HenryLiangzy@gmail.com>
2016-12-06 20:13:13 +0800

    add more details in main function

---
+++ ATM project/linklist.h
@@ -0,0 +1,39 @@
+#pragma once
+typedef struct custinfo
+{
+	char account[4];
+	char name[16];
+	char password[6];
+	float balance;
+	char level;
+	struct custinfo *next;
+}custinfo;
+
+
+//used for delete the linklist
+void listDelete(custinfo *head)
+{
+	custinfo *next, *current;
+	current = head;
+	while (current != NULL)
+	{
+		next = current->next;
+		free(current);
+		current = next;
+	}
+	//printf("list delete complete\n");
+}
+
+
+custinfo *searchByindex(custinfo *head, int n)
+{
+	custinfo *current;
+	int i = 1;
+	current = head;
+	while ((i < n) && (current != NULL))
+	{
+		current = current->next;
+		i++;
+	}
+	return current;
+}
--- ATM project/main.cpp
+++ ATM project/main.cpp
@@ -11,53 +11,48 @@ Last modification: Dec 5, 2016
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include "linklist.h"
 
-typedef struct {
-	int id;
-	char name[16];
-	char password[8];
-	float balance;
-	char level;
-}bankCount;
-//define the structe bankCount
 
-int login()
+int login(custinfo *head)
 {
 	//here is for the login function
 	//login function will return the account ID back to the main function
 }
 
-void check(int id)
+void check(int id, custinfo *head)
 {
 	//
 }
 
-void save(int id)
+void save(int id, custinfo *head)
 {
 	//
 }
 
 
-void withdraw(int id)
+void withdraw(int id, custinfo *head)
 {
 	//
 }
 
-void transfer(int id)
+void transfer(int id, custinfo *head)
 {
 	//
 }
 
-void quit()
+void quit(custinfo *head)
 {
 	//quit function used to end the program
+	listDelete(head);
 	exit(0);
+
 }
 
 
 
 
-void function_list(int id)
+void function_list(int id, custinfo *head)
 {
 	//here is for the function_list
 	//input the function number and jump to that funcion
@@ -86,16 +81,16 @@ void function_list(int id)
 
 		switch (*choose)
 		{
-		case 1:check(id);
+		case 1:check(id, head);
 			break;
-		case 2:save(id);
+		case 2:save(id, head);
 			break;
-		case 3:withdraw(id);
+		case 3:withdraw(id, head);
 			break;
-		case 4:transfer(id);
+		case 4:transfer(id, head);
 			break;
 		case 5:free(choose);
-				quit();
+				quit(head);
 			break;
 		default:printf("Please input the number 1~5\n");
 			break;
@@ -106,13 +101,43 @@ void function_list(int id)
 void main()
 {
 	int id;
-	id = login();
+	FILE *fp1;
+	struct custinfo *node1, *node2, *head;
+	struct custinfo temp;
+	fp1 = fopen("account.txt", "r");
+	if (fp1 != NULL)
+	{
+		node1 = (struct custinfo *)malloc(sizeof(struct custinfo));
+		fscanf(fp1, "%s %s %s %f %c", node1->account, node1->name, node1->password, &node1->balance, &node1->level);
+		head = node1;
+		printf("%s %s %s %f %c\n", node1->account, node1->name, node1->password, node1->balance, node1->level);
+		while (fscanf(fp1, "%s %s %s %f %c", temp.account, temp.name, temp.password, &temp.balance, &temp.level) != EOF)
+		{
+			node2 = (struct custinfo *)malloc(sizeof(struct custinfo));
+			//fscanf(fp1, "%d %s %d %f %c", &node2->id, node2->name, &node2->password, &node2->balance, &node2->level);
+			strcpy(node2->account, temp.account);
+			strcpy(node2->name, temp.name);
+			strcpy(node2->password, temp.password);
+			node2->balance = temp.balance;
+			node2->level = temp.level;
+			printf("%s %s %s %f %c\n", node2->account, node2->name, node2->password, node2->balance, node2->level);
+			node1->next = node2;
+			node1 = node2;
+		}
+		node2 = NULL;
+		fclose(fp1);
+	}
+	else
+	{
+		printf("file not exist\n");
+	}
+	id = login(head);
 	while (id != 0)
 	{
-		function_list(id);
+		function_list(id, head);
 	}
 	if (id == 0)
 	{
-		quit();
+		quit(head);
 	}
 }
